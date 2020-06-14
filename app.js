b7324b387184d70af3236c44d7026ea0ed49c10e
knoozfatima <knoozkk.edu@gmail.com>
2020-06-14 16:40:18 +0500

    Assignment#5

---
+++ app.js
@@ -0,0 +1,205 @@
+//Assignment #1 Task#1
+// alert("Hello ");
+//Assignment #1 Task#2
+// alert("Error!Please enter a valid password");
+//Assignment #1 Task#3
+// alert("Welcome to JS land...\nHappy Coding!");
+//Assignment #1 Task#5 mentioned in browser console
+//Assignment #1 Task#6
+// alert("Hellow viewer");
+//Assignment #2 Task#1
+var username;
+//Assignment #2 Task#2
+var myName="Knooz fatima";
+//Assignment #2 Task#3(a)
+var message;
+//Assignment #2 Task#3(b)
+message="Hello World";
+//Assignment #2 Task#3(c)
+// alert(message);
+//Assignment #2 Task#4
+// var name="knooz fatima";
+// var age="18 years old";
+// var certificate="Certified Mobile Application Developer";
+// alert(name+"\n"+age+"\n"+certificate);
+//Assignment #2 Task#5
+var pizza="PIZZA";
+// alert(pizza.slice(0,5)+"\n"+pizza.slice(0,4)+"\n"+pizza.slice(0,3)+"\n"+pizza.slice(0,2)+"\n"+pizza.slice(0,1));
+//Assignment #2 Task#6
+// var email="knoozkk.du@gmail.com";
+// alert("My email address is "+email);
+//Assignment #2 Task#7
+// var book="A smarter way to learn JavaScript";
+// alert(book);
+//Assignment #2 Task#8
+// document.write("Yah! I can write HTML content through JavaScript");
+// var string="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(string);
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+//Assignment #3 Task#1
+// var age =18;
+// alert("I am "+age+" years old");
+//Assignment #3 Task#2
+// var Number=12;
+// alert("You have visited this site "+ Number +" times");
+//Assignment #3 Task#3
+// var birthYear=2001;
+// document.write("My birth Year is "+birthYear ,"<br>","Data type of my declared variable is number");
+//Assignment #3 Task#4
+// var visitorName="John Doe";
+// var ProductTitle="T-shirt(s)";
+// var quantity=5;
+// document.write( visitorName," ordered ",quantity," ",ProductTitle," on XYZ Clothing store");
+//Assignment #4 Task#1
+// var a, b,  c;
+//Assignment #4 Task#2
+// var a,b1,c303,_d,$d;
+// var 1f,#w,101,var,*kk;
+//Assignment #4 Task#3(b)
+// document.write("Variable names can only contain , numbers, $ and _ .For example : $my_1stVariable","<br> ");
+// document.write("Variable must begin with a letter, $ or _ . For example : $name,_ name or name","<br> ");
+// document.write("Variable names are case sensitive","<br> ");
+// document.write("Variable names should not be JS keywords","<br> " );
+//Assignment #5 Task#1
+// var a=+prompt();
+// var b=+prompt();
+// var c= a+b;
+// document.write("Sum of ",a," and ",b," is ",c);
+//Assignment #5 Task#2(a)
+// var a=+prompt();
+// var b=+prompt();
+// var c= a-b;
+// document.write("Subtraction of ",a," and ",b," is ",c);
+// //Assignment #5 Task#2(b)
+// var a=+prompt();
+// var b=+prompt();
+// var c= a*b;
+// document.write("Multiplicatin of ",a," and ",b," is ",c);
+// //Assignment #5 Task#2(c)
+// var a=+prompt();
+// var b=+prompt();
+// var c= a/b;
+// document.write("Division of ",a," and ",b," is ",c);
+// //Assignment #5 Task#2(d)
+// var a=+prompt();
+// var b=+prompt();
+// var c= a%b;
+// document.write("Modulus of ",a," and ",b," is ",c);
+// //Assignment #5 Task#3
+// var a;
+// document.write("Value after variable declaration is "+a,"<br>");
+// a=5;
+// document.write("Initial value: "+a,"<br>");
+// ++a;
+// document.write("Value after increment is :"+a,"<br>");
+// a=a+7;
+// document.write("Value after addition is: "+a+"<br>");
+// --a;
+// document.write("Value after decrement is: "+a+"<br>");
+// a=a%3;
+// document.write("The remainder is: "+a+"<br>");
+// //Assignment #5 Task#4
+// var ticket=600;
+// document.write("Total cost to buy 5 tickets to a movie is ",5*ticket, " PKR");
+// //Assignment #5 Task#5
+// document.write("Table of 4","<br>");
+// for(var a=1;a<=10;a++)
+// {
+//     document.write("4x",a,"=",4*a,"<br>");
+// }
+//Assignment #5 Task#6
+// var c=25;
+// document.write(c ,"°C is ",(c*9/5)+32,"°F <br>");
+// var f=70;
+// document.write(f ,"°F is ",(f-32)*5/9,"°C");
+//Assignment #5 Task#7
+// var item1=650;
+// var item2=100;
+// var quantityOFItem1=3;
+// var quantityOfItem2=7;
+// var shippingCharges=100;
+// document.write("Price of item 1 is ",item1,"<br>");
+// document.write("Quantity of item 1 is ",quantityOFItem1,"<br>");
+//  document.write("Price of item 2 is ",item2,"<br>");
+//  document.write("Quantity of item 2 is ",quantityOfItem2,"<br>");
+
+// document.write("Shipping Charges ",shippingCharges,"<br>");
+// document.write("Total cost of your order is ",item1*quantityOFItem1+item2*quantityOfItem2+shippingCharges);
+//Assignment #5 Task#8
+// var totalMarks=980;
+// var marksObtained=804;
+// document.write("Total marks: ",totalMarks,"<br>");
+// document.write("Marks obtained: ",marksObtained,"<br>");
+// document.write("Percentage: ",(marksObtained/totalMarks)*100);
+//Assignment #5 Task#9
+// var uS=104.80;
+// var saudi=28;
+// document.write("Total Currency in PKR: ",10*uS+25*saudi);
+//Assignment #5 Task#10
+// var a=2;
+// var b=((a+5)*10)/2;
+//Assignment #5 Task#11
+// var cYear=2016;
+// var bYear=1992;
+// document.write("Current Year: ",cYear,"<br>");
+// document.write("Birth Year: ",bYear,"<br>");
+// document.write("Your age is: ",cYear-bYear,"<br>");
+//Assignment #5 Task#12
+// var radius=20;
+// document.write("Radius of a Circle is: ",radius,"<br>");
+// document.write("The circumference is: ",2*3.14*radius,"<br>");
+// document.write("The area is: ",3.142*radius*radius);
+//Assignment #5 Task#13
+// var fs="chocolate chip";
+// var cage=15;
+// var maxage=65;
+// var amount=3;
+// document.write("Favourite Snack: ",fs,"<br>");
+// document.write("Current age: ",cage,"<br>");
+// document.write("Estimated Maximum Age: ",maxage,"<br>");
+// document.write("Amount of snacks per day: ",amount,"<br>");
+// document.write("You will need ",(maxage-cage)*3 ," chocolate chip to last you untill the ripe old age of ",maxage);
+//Assignment #6 Task#1
+// var a=10;
+// document.write("Result:","<br>");
+// document.write("The value of a is: ",a,"<br>","<br>");
+// document.write(".......................................","<br>","<br>","<br>");
+// document.write("The value of ++a is: ",++a,"<br>");
+// document.write("Now the value of a is: ",a,"<br>","<br>","<br>");
+// document.write("The value of a++ is: ",a++,"<br>");
+// document.write("Now the value of a is: ",a,"<br>","<br>","<br>");
+// document.write("The value of --a is: ",--a,"<br>");
+// document.write("Now the value of a is: ",a,"<br>","<br>","<br>");
+// document.write("The value of a-- is: ",a--,"<br>");
+// document.write("Now the value of a is: ",a,"<br>","<br>","<br>");
+//Assignment #7 Task#1
+// var a=2,b=1;
+// var result=--a- --b+ ++b+b--;
+//            //1 - 0 +  1 +1
+// document.write("a is ",a,"<br>");
+// document.write("b is ",b,"<br>");
+// document.write("result is ",result,"<br>");
+//Assignment #8 Task#1
+// var name =prompt();
+// alert("Hello "+name);
+//Assignment #9 Task#1
+// var numbe =+prompt();
+
+// for(var a=1;a<=10;a++)
+// {
+//     document.write(number,"X",a,"=",a*number,"<br>");
+// }
+//Assignment #9 Task#2
+// var a=+prompt();
+// var b=+prompt();
+// var c=+prompt();
+// var total=100;
+// var d=+prompt();
+// var e=+prompt();
+
+// var f=+prompt();
+// document.write(a,"   ",total,"   ",d,"   ",(d/total)*100,"%","<br>");
+// document.write(b,"   ",total,"   ",e,"   ",(e/total)*100,"%","<br>");
+// document.write(c,"   ",total,"   ",f,"   ",(f/total)*100,"%","<br>");
+// document.write(total*3+" "+" "+d+e+f+" "+ (d+e+f)/300+"%");
+
